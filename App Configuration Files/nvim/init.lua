e03ea2458561c1dcd2e9ce7f0c59ec99bddb4995
Are Digranes <ared@runbox.no>
2026-04-27 14:32:26 +0200

    minor changes

--- App Configuration Files/nvim/init.lua
+++ App Configuration Files/nvim/init.lua
@@ -171,7 +171,7 @@ require("lualine").setup {
     	lualine_c = { {
 		'filename',
 	        symbols = {
-		    modified = '',
+		    modified = '•',
 		    readonly = '󱀰',
 		    unnamed = '--',
 		    newfile = '󰻭', 
