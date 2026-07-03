5aa2506e8dd813b3922afc36cede26c12d73c3d6
Are Digranes <ared@runbox.no>
2026-07-03 21:02:47 +0200

    steam and various

---
+++ Other/steam-arm64/get.zsh
@@ -0,0 +1,67 @@
+acf_to_json() {
+    local -a raw_lines=("$@")
+    local -a lines
+    local l
+
+    # Wrap multi-root VDF content in a synthetic root object
+    lines+=('"__root__"')
+    lines+=('{')
+    for l in "${raw_lines[@]}"; do
+        lines+=($'\t'"$l")
+    done
+    lines+=('}')
+
+    local n=${#lines[@]}
+    local output=""
+    local next
+    local key value
+
+    local re_single=$'^(\t+".+")\t\t(".*")$'
+    local re_start_of_object=$'^\t+".+"$'
+
+    for ((i = 2; i <= n; i++)); do
+        line="${lines[i]}"
+        next="${lines[i + 1]}"
+
+        if [[ "$line" =~ $re_single ]]; then
+            key="${match[1]}"
+            value="${match[2]}"
+            output+="${key}: ${value}"
+
+            if ((i + 1 <= n)) && [[ "$next" == *"}" ]]; then
+                output+=$'\n'
+            else
+                output+=$',\n'
+            fi
+
+        elif [[ "$line" == $'\t'* && "$line" == *"}" ]]; then
+            output+="$line"
+
+            if ((i + 1 <= n)) && [[ "$next" == *"}" ]]; then
+                output+=$'\n'
+            else
+                output+=$',\n'
+            fi
+
+        elif [[ "$line" =~ $re_start_of_object ]]; then
+            output+="${line}:"$'\n'
+
+        else
+            output+="${line}"$'\n'
+        fi
+    done
+
+    print -r -- "$output"
+}
+acf_data=$(curl -sf "https://client-update.steamstatic.com/steam_client_osx")
+acf_lines=("${(f)acf_data}")
+json_output=$(acf_to_json "${acf_lines[@]}")
+zip_file=$(jq .osx.appdmg_osx.file <<<$json_output)
+temp_dir=$(mktemp -d)
+curl -L "https://client-update.steamstatic.com/$zip_file" -o "$temp_dir/appdmg_osx.zip"
+unzip -q "$temp_dir/appdmg_osx.zip" -d "$temp_dir"
+cp -R "$temp_dir/Steam.app" "/Applications/"
+xattr -dr com.apple.quarantine "/Applications/Steam.app"
+package_dir="$HOME/Library/Application Support/Steam/package"
+mkdir -p "$package_dir"
+echo "publicbeta" >"$package_dir/beta"
